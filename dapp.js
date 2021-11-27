25c28d8f14c0ebf75db62ccc75eca6325441fb21
David Bendeck <dabap504@gmail.com>
2021-11-26 21:29:54 -0600

    this copies the frontend from my main proyect

---
+++ dapp.js
@@ -0,0 +1,172 @@
+var lotjson;
+var hash;
+
+async function loadJSON() {
+  const response = await fetch('./contracts/LotteryReduced.json');
+  lotjson = await response.json();
+}
+
+loadJSON();
+
+async function selectedAddress() {
+  const accounts = await ethereum.request({
+    method:
+      'eth_requestAccounts'
+  });
+  if (typeof accounts !== 'undefined') {
+    return accounts[0].toLowerCase();
+  }
+  return "";
+}
+
+ethereum.on('accountsChanged', function (accounts) {
+  getInfo();
+})
+
+window.addEventListener('load', function () {
+  if (typeof window.ethereum !== 'undefined') {
+    let mmDetected = document.getElementById('mm-detected')
+    mmDetected.innerHTML = "Metamask Has Been Detected!"
+  }
+  else {
+    this.alert("You need to install Metamask!")
+  }
+  getInfo();
+})
+
+const mmEnable = document.getElementById('mm-connect')
+var accounts;
+mmEnable.onclick = async () => {
+  accounts = await ethereum.request({
+    method:
+      'eth_requestAccounts'
+  });
+
+  const mmCurrentAccount = document.getElementById('mm-current-account');
+  mmCurrentAccount.innerHTML = accounts[0];
+}
+
+async function getInfo() {
+  var SCResponse;
+  
+  const participant1 = document.getElementById("lo-participant1");
+  const participant2 = document.getElementById("lo-participant2");
+  const lotteryCount = document.getElementById("lo-lotteryCount");
+  const winner = document.getElementById("lo-winner");
+  const status = document.getElementById("lo-status");
+
+  var web3 = new Web3(window.ethereum)
+  const networkId = await web3.eth.net.getId();
+
+  const lottery = new web3.eth.Contract
+    (lotjson.abi, lotjson.networks[networkId].address);
+
+  try {
+    SCResponse = await lottery.methods.requestData().call();
+  } catch (error) {
+    document.getElementById("lo-participant1").innerHTML = "---";
+    document.getElementById("lo-participant2").innerHTML = "---";
+    document.getElementById("lo-lotteryCount").innerHTML = "---";
+    document.getElementById("lo-winner").innerHTML = "---";
+
+    return false;
+  }
+
+  document.getElementById("lo-participant1").innerHTML = "Participant 1: " + SCResponse[1];
+  document.getElementById("lo-participant2").innerHTML = "Participant 2: " +SCResponse[2];
+  document.getElementById("lo-lotteryCount").innerHTML = "Lottery #: " +SCResponse[4];
+  document.getElementById("lo-winner").innerHTML = "Winner: " + SCResponse[3];
+  
+  let statusDisplay;
+  
+  switch (parseInt(SCResponse[0])) {
+    case 0:
+      statusDisplay = "A lottery has started!";
+      break;
+    case 1:
+      statusDisplay = "One bet received!";
+      break;
+    case 2:
+      statusDisplay = "Two bets received!";
+      break;
+    case 3:
+      statusDisplay = "Electing a winner...";
+      break;
+    case 4:
+      statusDisplay = "Winner elected, claim your prize!";
+      break;
+    
+    default:
+      statusDisplay = "You should not be seeing this ...."
+      break;
+    
+  }
+  document.getElementById("lo-status").innerHTML = statusDisplay;
+  return true;
+}
+
+const loBet = document.getElementById("lo-bet");
+loBet.onclick = async () => {
+
+  if (!window.ethereum) {
+    alert("No MetaMask detected");
+    return;
+  }
+
+  var web3 = new Web3(window.ethereum)
+  const networkId = await web3.eth.net.getId();
+
+  // console.log(networkId);
+  // console.log(lotjson.networks[networkId].address);
+  // console.log(await selectedAddress());
+
+  const lottery = new web3.eth.Contract
+    (lotjson.abi, lotjson.networks[networkId].address);
+  
+  await lottery.methods
+  .placeBet()
+  .send({ from: await selectedAddress(), value: 1e9 });
+
+  getInfo();
+}
+
+const loPrize = document.getElementById("lo-prize");
+loPrize.onclick = async () => {
+
+  if (!window.ethereum) {
+    alert("No MetaMask detected");
+    return;
+  }
+
+  var web3 = new Web3(window.ethereum)
+  const networkId = await web3.eth.net.getId();
+
+  // console.log(networkId);
+  // console.log(lotjson.networks[networkId].address);
+  // console.log(await selectedAddress());
+
+  const lottery = new web3.eth.Contract
+    (lotjson.abi, lotjson.networks[networkId].address);
+  
+  await lottery.methods
+  .requestPot()
+  .send({ from: await selectedAddress() });
+
+  getInfo();
+}
+/*
+const ckSign = document.getElementById("lo-bet");
+ckSign.onclick = async () => {
+
+  var web3 = new Web3(window.ethereum)
+  const networkId = await web3.eth.net.getId();
+
+  const contractKeeper = new web3.eth.Contract
+    (lotjson.abi, lotjson.networks[networkId].address);
+
+  const response = await contractKeeper.methods
+    .signContract("0x" + hash.toString(CryptoJS.enc.Hex)).send({ from: await selectedAddress() });
+
+  verifyContract();
+}
+*/
